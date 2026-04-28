8d0f992eb7d152bd1bcf24bfd8de073580d2c9ad
youssna <nasefyoussef@outlook.com>
2026-04-28 12:23:06 +0200

    Update database connection credentials and host

--- basicfit-training/bdd/bdd.php
+++ basicfit-training/bdd/bdd.php
@@ -1,9 +1,9 @@
 <?php
 
 try {
-    $users = "root";
-    $pass = "root"; 
-    $bdd = new PDO("mysql:host=localhost;dbname=fitconnect;charset=utf8", $users, $pass);
+    $users = "jfrancois";
+    $pass = "password!75"; 
+    $bdd = new PDO("mysql:host=192.168.30.130;dbname=fitconnect;charset=utf8", $users, $pass);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Erreur ! : " . $e->getMessage() . "<br/>";
